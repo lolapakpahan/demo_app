dba652f1f3375fa7bcc348a6c432577593cab43b
Lola Pakpahan <lola@lola.(none)>
2014-06-09 10:16:55 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tes::Application.config.secret_token = '754370cb6a109b7c54fbe6948fdc3d4c8cd82e8f3e5bb2189f534b40e15c12a127c15466834cfaf2b2ab7a43adb07b2876ff3adc0fcace2713c0cd7f2b5f47cf'
